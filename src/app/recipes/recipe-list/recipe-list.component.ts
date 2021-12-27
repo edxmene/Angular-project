4ba62f993ac9d2050619244b46af12cde6933f1f
Edxon Meneses <edxon.meneses@gmail.com>
2021-12-27 16:39:31 -0500

    Recipes component added with some temporary features

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe 1', 'This is a test recipe 1','https://i0.hippopx.com/photos/653/692/24/recipe-chicken-bacon-meat-preview.jpg'),
+    new Recipe('Test Recipe 2', 'This is a test recipe 2','https://i0.hippopx.com/photos/653/692/24/recipe-chicken-bacon-meat-preview.jpg'),
+    new Recipe('Test Recipe 3', 'This is a test recipe 3','https://i0.hippopx.com/photos/653/692/24/recipe-chicken-bacon-meat-preview.jpg')
+  ];
 
   constructor() { }
 
